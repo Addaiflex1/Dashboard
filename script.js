8f2b36dc3e9763a165110a7cd75ab2dad648929a
Addaiflex <addaiflex3@gamil.com>
2023-12-27 02:53:29 +0000

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const body = document.querySelector("body"),
+   sidebar = body.querySelector(".sidebar"),
+   toggle = body.querySelector(".toggle"),
+   searchBtn = body.querySelector(".search-box"),
+   modeSwitch = body.querySelector(".toggle-switch"),
+   modeText = body.querySelector(".mode-text");
 
+// Toggle sidebar
+toggle.addEventListener("click", () => {
+   sidebar.classList.toggle("close");
+});
+
+// Show search input box
+searchBtn.addEventListener("click", () => {
+   searchBtn.classList.toggle("active");
+});
+
+// Check and set initial mode text
+if(body.classList.contains("dark")){
+   modeText.innerHTML = "Light Mode"
+}else{
+   modeText.innerHTML = "Dark Mode"
+}
+
+// Toggle dark mode
+modeSwitch.addEventListener("click", () => {
+   body.classList.toggle("dark");
+   modeText.textContent = body.classList.contains("dark") ? "Light Mode" : "Dark Mode";
+});
